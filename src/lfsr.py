e396898b94706ed30b29a90e42888690b91615f2
Yishai Gantz <gantz.yishai@gmail.com>
2026-01-03 23:57:05 +0200

    my_commit

--- src/lfsr.py
+++ src/lfsr.py
@@ -45,6 +45,9 @@ class LFSR32:
             Next state of the LFSR (32-bit unsigned integer).
         """
         # TODO: Implement LFSR step
+        fb = ((state >> 31) & 1) ^ ((state >> 21) & 1) ^ ((state >> 1) & 1) ^ ((state >> 0) & 1)  # XOR calculation for the feedback bit
+        next_state = state >> 1 | (fb << 31) # shift right and insert feedback bit at MSB
+        return next_state & 0xFFFFFFFF # clearing bits upper than 32 bits
         # Hint: XOR taps at positions 32, 22, 2, 1 (0-indexed: 31, 21, 1, 0)
         # Shift right and insert feedback bit at MSB
         raise NotImplementedError("Students must implement _lfsr32")
@@ -71,4 +74,7 @@ class LFSR32:
             A pseudo-random integer in the specified range.
         """
         # TODO: Implement random integer generation using _next()
+        scale = max_val - min_val + 1 # range size
+        random_value = self._next() % scale + min_val # give random number in scale
+        return random_value
         raise NotImplementedError("Students must implement random_int")
